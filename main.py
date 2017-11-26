a39c04223d04ac0a9191ad04d66fc560e9bb4e60
vishalkg <vishal.gupta4081@gmail.com>
2017-11-26 02:01:26 -0500

    RBF SVM completed

--- main.py
+++ main.py
@@ -9,15 +9,11 @@ from sklearn.svm import SVC
 from sklearn.model_selection import StratifiedShuffleSplit
 from sklearn.model_selection import GridSearchCV
 from sklearn.preprocessing import StandardScaler
-
-mySVM = SVC(kernel='rbf')
+from sklearn.metrics import precision_recall_fscore_support
 
 def load_data(csv_data,ratio=0.1):
     # Read the csv nd extract all the required info
     dataframe = pd.read_csv(csv_data)
-    dataframe['NormalisedAamount'] = StandardScaler().fit_transform(dataframe['Amount'].reshape(-1, 1))
-    dataframe = dataframe.drop(['Time','Amount'],axis=1)
-    dataframe.head()
     num_pos = dataframe[dataframe['Class']==1].shape[0]
     num_neg = dataframe.shape[0]-num_pos
 
@@ -33,41 +29,68 @@ def load_data(csv_data,ratio=0.1):
 
     return dataframe
 
-# data is pandas dataframe
-# Normal split, no stratified split
-# Bound to fail due to skewness of data
-def get_tr_tst(data,ratio=0.10):
-    temp = data.as_matrix()
-    indices = np.random.permutation(data.shape[0])
+"""
+Data set has a column 'Amount', which is not normalised
+So, we normalise the 'Amount' column and drop the original 'Amount' Column
+Further, since the data is highly skewed, we undersample it 
+"""
+def preprocess_FollowUnderSampling(data):
+    data['NormalisedAmount'] = StandardScaler().fit_transform(data['Amount'].values.reshape(-1, 1))
+    data = data.drop(['Time','Amount'],axis=1)
+
+    # Undersampling Steps
+    fraud_count = len(data[data.Class==1])
+    fraud_indices = data[data.Class==1].index
+    notFraud_indices = data[data.Class==0].index
+    rand_notFraud_indices = notFraud_indices[np.random.permutation(len(notFraud_indices))]
+    rand_notFraud_indices_undersampled = rand_notFraud_indices[0:3*fraud_count]
+    undersampled_indices = np.concatenate([fraud_indices,rand_notFraud_indices_undersampled])
+    undersampled_data = data.iloc[undersampled_indices,:]
+    X_undersampled = undersampled_data.iloc[:,undersampled_data.columns!='Class']
+    Y_undersampled = undersampled_data.iloc[:,undersampled_data.columns=='Class']
+    return X_undersampled,Y_undersampled
+
+"""
+X,Y are pandas dataframes
+"""
+def get_tr_tst(X,Y,ratio=0.10):
+    X = X.as_matrix()
+    Y = Y.as_matrix()
+    indices = np.random.permutation(X.shape[0])
     split_index = int(ratio*indices.size)
-    tst_data = temp[indices[0:split_index],0:-1]
-    tst_lb = temp[indices[0:split_index],-1]
-    tr_data = temp[indices[split_index:],0:-1]
-    tr_lb = temp[indices[split_index:],-1]
+    tst_data = X[indices[0:split_index],:]
+    tst_lb = Y[indices[0:split_index]]
+    tst_lb.shape = (tst_lb.size,)
+    tr_data = X[indices[split_index:],:]
+    tr_lb = Y[indices[split_index:]]
+    tr_lb.shape = (tr_lb.size,)
     print("Statitics:")
     print(". # of train/test samples: {}/{}".format(tr_lb.size,tst_lb.size))
     return tr_data,tr_lb,tst_data,tst_lb
 
-def rbf_svm(tr_feat,tr_lb,tst_feat,tst_lb):
-#    mySVM = SVC(kernel='rbf')
-    C_range = np.logspace(1,4,4)
-    Gamma_range = np.logspace(1,4,4)
-    param_grid = dict(gamma=Gamma_range, C=C_range)
-    cv = StratifiedShuffleSplit(n_splits=10, test_size=0.1, random_state=32)
+def rbf_svm(tr_data,tr_lb,tst_data,tst_lb):
+    mySVM = SVC(kernel='rbf')
+    C_range = [0.01,0.1,1,10,100,1000,10000]
+    param_grid = dict(C=C_range)
+    #param_grid
+    cv = StratifiedShuffleSplit(n_splits=5, test_size=0.1)
     clf = GridSearchCV(mySVM, param_grid=param_grid, cv=cv, n_jobs=-1)
-    clf.fit(tr_feat,tr_lb)
-    print("The best parameters are %s with a score of {0:.2f}".format(clf.best_params_, clf.best_score_))
-
-    pred = clf.predict(tst_feat) - np.array(tst_lb)
+    clf.fit(tr_data,tr_lb)
+    message = ("*MESSAGE* Best C: {}, Score: {}".format(clf.best_params_['C'],clf.best_score_))
+    print(message)
+    pred = clf.predict(tst_data)
     count = 0
-    for i in pred:
-        if i==0: count = count+1
-    print('Accuracy = ' + str(count*100/len(tst_feat)))
+    for i in range(pred.size):
+        if pred[i]==tst_lb[i]: count += 1
+    print("*MESSAGE* Accuracy: {}".format(count/tst_lb.size))
+    precision,recall,fscore = precision_recall_fscore_support(tst_lb,pred,average='binary')[0:3]
+    print("*MESSAGE* Precision: {}, Recall: {}, F-score: {}".format(precision,recall,fscore))
 
 def main():
     csv_data = "creditcard.csv"
     data = load_data(csv_data,0.1)
-    tr_data,tr_lb,tst_data,tst_lb = get_tr_tst(data,0.1)
+    X,Y = preprocess_FollowUnderSampling(data)
+    tr_data,tr_lb,tst_data,tst_lb = get_tr_tst(X,Y,0.1)
     rbf_svm(tr_data,tr_lb,tst_data,tst_lb)
 
 
